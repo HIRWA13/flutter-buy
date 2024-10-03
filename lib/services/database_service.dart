33577dc73f70bbe64cc1966f10a562e5e2c98269
Junior Hirwa <iamhirwejr@gmail.com>
2024-10-03 20:19:33 +0200

    feat: update the database service

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -1,5 +1,6 @@
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
+import '../models/user_model.dart';
 
 class DatabaseService {
   static final DatabaseService instance = DatabaseService._init();
@@ -14,23 +15,29 @@ class DatabaseService {
     return _database!;
   }
 
-  Future<Database> _initDB(String filepatth) async {
-    final dbPath = await getDatabasesPath();
-    final path = join(dbPath, filepatth);
+  Future<Database> _initDB(String filepath) async {
+    try {
+      final dbPath = await getDatabasesPath();
+      final path = join(dbPath, filepath);
+      print('db path: $path');
 
-    return await openDatabase(path, version: 1, onCreate: _createDB);
+      return await openDatabase(path, version: 1, onCreate: _createDB);
+    } catch (e) {
+      print('Error initializing database: $e');
+      rethrow;
+    }
   }
 
   Future _createDB(Database db, int version) async {
-    await db.execute('''CREATE TABLE USERS (
+    await db.execute('''CREATE TABLE users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         username TEXT NOT NULL,
         password TEXT NOT  NULL
       )''');
 
     await db.execute('''
-      CREATE TABLE PRODUCT (
-      id PRIMARY KEY AUTOINCREMENT,
+      CREATE TABLE products (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
       title TEXT NOT NULL,
       description TEXT,
       imageUrl TEXT NOT NULL,
@@ -41,5 +48,23 @@ class DatabaseService {
   Future close() async {
     final db = await instance.database;
     db.close();
+  }
+
+  Future<int> createUser(User user) async {
+    final db = await instance.database;
+    return db.insert('users', user.toMap());
+  }
+
+  Future<User?> loginUser(String username, String password) async {
+    final db = await instance.database;
+    final result = await db.query('users',
+        where: 'username = ? AND password = ?',
+        whereArgs: [username, password]);
+
+    if (result.isNotEmpty) {
+      return User.fromMap(result.first);
+    } else {
+      return null;
+    }
   }
 }
